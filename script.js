185e0fd9d1b7bccd461fff78d76675436fd3b92c
Lakshman Virijala <113916846+roy4262@users.noreply.github.com>
2024-10-25 22:02:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Full Stack Developer","FrontEnd Developer", "Backend Developer"],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
